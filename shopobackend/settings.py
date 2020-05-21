86fd6c7a86fddc33ecbec601ca16167ec6e7bebe
Sergio Andres Florez <sergio.florez@grability.com>
2020-05-21 13:44:17 -0500

    add postgres config

--- shopobackend/settings.py
+++ shopobackend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'shopobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shopo',
+        'USER': 'shopo',
+        'PASSWORD': 'shopo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
