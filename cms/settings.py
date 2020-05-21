a167714489719f3065c1b4bb7c7a6037a46c2d03
Sergio Andres Florez <sergio.florez@grability.com>
2020-05-21 13:18:54 -0500

    remove quickstart app

--- cms/settings.py
+++ cms/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shopo',
+        'USER': 'shopo',
+        'PASSWORD': 'shopo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
